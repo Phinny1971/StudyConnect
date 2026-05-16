86c970714212fd24272b0cab1dc078d586f1fd78
Phinny1971 <phinnynoel@gmail.com>
2026-05-16 17:41:58 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,10 +15,11 @@
 <?php
 
 
-$host = "sql101.infinityfree.com"; 
-$dbname = "if0_41864403_studyconnect";
-$username = "if0_41864403"; 
-$password = "Study2025";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 /*
 $host = "localhost";
@@ -27,7 +28,8 @@ $username = "StudyConnect";
 $password = "Study@2025";
 */
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+
 if ($conn->connect_error) {
   http_response_code(500);
   die("Connection failed: " . $conn->connect_error);
