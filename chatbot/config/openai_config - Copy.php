68d194a11047fad2d97272577dd714687d121009
Phinny1971 <phinnynoel@gmail.com>
2026-05-16 22:12:43 +0530

    Add files via upload

---
+++ chatbot/config/openai_config - Copy.php
@@ -0,0 +1,5 @@
+<?php
+
+define('OPENAI_API_KEY', 'AIzaSyDK6Z3gL8gRd10WAdAYNEdJax_INcBzsW8');
+
+?>
--- chatbot/config/openai_config.php
+++ chatbot/config/openai_config.php
@@ -2,4 +2,5 @@
 
 //$API_KEY = getenv('GEMINI_API_KEY');
 $API_KEY = getenv('OPENROUTER_API_KEY');
+
 ?>
