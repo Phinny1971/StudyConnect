cb4a37ecb17d8b71046b87a3776c374c771d367c
Phinny1971 <phinnynoel@gmail.com>
2026-05-16 21:31:17 +0530

    Update send_message.php

--- chatbot/api/send_message.php
+++ chatbot/api/send_message.php
@@ -159,8 +159,14 @@ $payload = [
 
 ];
 
+/*
 $apiUrl =
 "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
+. $API_KEY;
+*/
+
+$apiUrl =
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
 . $API_KEY;
 
 $ch = curl_init();
