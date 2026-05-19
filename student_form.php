8b2bc550dfb7089e9a3ed2a1d87cbba5cc97f668
Phinny1971 <phinnynoel@gmail.com>
2026-05-19 20:27:02 +0530

    Update student_form.php

--- student_form.php
+++ student_form.php
@@ -14,10 +14,11 @@ if (!isset($_SESSION["user_id"])) {
     exit();
 }
  */
-$host = "sql101.infinityfree.com"; 
-$dbname = "if0_41864403_studyconnect";
-$username = "if0_41864403"; 
-$password = "Study2025";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 /*
 $host = "localhost";
@@ -26,10 +27,9 @@ $username = "StudyConnect";
 $password = "Study@2025";
 */
 
-
-
 // Create connection
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+//$conn = new mysqli($host, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
