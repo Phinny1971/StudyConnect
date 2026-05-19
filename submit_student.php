75eb350ac224992bfa8c524994e8f86c568777a1
Phinny1971 <phinnynoel@gmail.com>
2026-05-19 21:15:23 +0530

    Update submit_student.php

--- submit_student.php
+++ submit_student.php
@@ -97,6 +97,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+
 function nullIfEmpty($value) {
     return ($value === '' || $value === null)
         ? null
