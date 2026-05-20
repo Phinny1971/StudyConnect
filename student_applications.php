aa31eea218783dea00a6c9e4a02da7e7c32a7c20
Phinny1971 <phinnynoel@gmail.com>
2026-05-20 18:24:02 +0530

    Update student_applications.php

--- student_applications.php
+++ student_applications.php
@@ -1,12 +1,12 @@
 <?php
 
-
 session_start();
 
-$host = "sql101.infinityfree.com"; 
-$dbname = "if0_41864403_studyconnect";
-$username = "if0_41864403"; 
-$password = "Study2025";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 /*
 $host = "localhost";
@@ -15,10 +15,12 @@ $username = "StudyConnect";
 $password = "Study@2025";
 */
 
-$conn = new mysqli($host, $username, $password, $dbname);
-if ($conn->connect_error) {
-  http_response_code(500);
-  die("Connection failed: " . $conn->connect_error);
+// Create connection
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+//$conn = new mysqli($host, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 
