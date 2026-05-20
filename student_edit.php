59a8056ce7056640549dca900ea83d25396adc02
Phinny1971 <phinnynoel@gmail.com>
2026-05-20 17:11:08 +0530

    Update student_edit.php

--- student_edit.php
+++ student_edit.php
@@ -1,9 +1,11 @@
  
 <?php
-$host = "sql101.infinityfree.com"; 
-$dbname = "if0_41864403_studyconnect";
-$username = "if0_41864403"; 
-$password = "Study2025";
+
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$database = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
 /*
 $host = "localhost";
@@ -12,8 +14,11 @@ $username = "StudyConnect";
 $password = "Study@2025";
 */
 
+// Create connection
+$conn = mysqli_connect($host, $user, $password, $database, $port);
+//$conn = new mysqli($host, $username, $password, $dbname);
 
-$conn = new mysqli($host, $username, $password, $dbname);
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
