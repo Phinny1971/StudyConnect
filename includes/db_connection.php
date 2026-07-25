a4ae5fd280a285f3b0b67219c25475e376b54d62
Phinny1971 <phinnynoel@gmail.com>
2026-07-25 15:40:58 +0530

    Add files via upload

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,20 +1,20 @@
 <?php
 
-/*
+
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $password = getenv('MYSQLPASSWORD');
 $database = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
-*/
 
+/*
 $host = "localhost";
 $dbname = "studyconnect";
 $username = "StudyConnect";
 $password = "Study@2025";
-
-$conn = new mysqli($host, $username, $password, $dbname);
-//$conn = mysqli_connect($host, $user, $password, $database, $port);
+*/
+//$conn = new mysqli($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 
 if ($conn->connect_error) {
